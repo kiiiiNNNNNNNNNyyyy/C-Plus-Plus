2582cff3a23040c72b6e8db3c57668171adab0dc
Arjun  Dass <ad.sam007@gmail.com>
2017-06-05 14:42:31 -0500

    Variables

--- Basic Programs/Program1.cpp
+++ Basic Programs/Program1.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 main(){
-    std::cout<<"Hey !! hwat is up!!";
+
+    int a = 4;
+    std::cout<<"Hey !! hwat is up!!" << a << std::endl; //allocating space in memory = 4 bits
+    std::cout << "Getting address of the variale: " << &a << std::endl;
+    a = 10;
+    std::cout << "Value of a: " << a << " and adress is still same: " << &a;
+
+    /*
+        variables cannot have the same name
+        variables cannot start with a number 
+        we cannot use space in variables name
+        variable should be self-descriptive
+        variables cannot have special characters keywords
+        variables should be nouns
+    */
+
 }
 
