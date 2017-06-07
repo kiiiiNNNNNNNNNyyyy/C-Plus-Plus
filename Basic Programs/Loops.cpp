fd38c8848470465c37cec98552a99e95149aac17
Arjun  Dass <ad.sam007@gmail.com>
2017-06-07 14:41:14 -0500

    continue and break

--- Basic Programs/Loops.cpp
+++ Basic Programs/Loops.cpp
@@ -31,4 +31,13 @@ main(){
         x++;
     }while(x < 20);
 
+    //break and continue
+    for(int i = 1; i <= 10; i++){
+        for(int j = 1;   j <= 10; j++){
+            std::cout.width(4);
+            std::cout << i*j;
+        }
+        std::cout << std::endl;
+    }
+
 }
