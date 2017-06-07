568be8d47f406f99f20fa216824db57149f96cdb
Arjun  Dass <ad.sam007@gmail.com>
2017-06-07 13:08:27 -0500

    Bitwise Operators

--- Basic Programs/Operators.cpp
+++ Basic Programs/Operators.cpp
@@ -35,4 +35,52 @@ main(){
     std::cout << " Is a >= b: " << (a >= b) << std::endl; //O - because its false  
     std::cout << " Is a <= b: " << (a <= b) << std::endl; //1 - because its true 
 
+    //LOGICAL OPERATORS
+
+    std::cout << "Is (7 > 5 && 5 !=10) = " <<  (7 > 5 && 5 !=10) << std::endl;
+    std::cout << "Is (7 > 5 || 5 ==10) = " <<  (7 < 5 || 5 ==10) << std::endl;
+
+    //BITWISE OPERATORS
+
+    /*
+        126 = (1 * 10 ^ 2)  + (2 * 10 ^ 1) + (6 * 10 ^ 0) = 126
+        1010 = (1 * 2 ^ 3) + (0 * 2 ^ 2) + (1 * 2 ^ 1) + (0 * 2 ^ 0) = 8 + 0 + 2 + 0 = 10 
+        101100 = (1 * 2 ^ 5) + (1 * 2 ^ 3) + (1 * 2 ^ 2) = 32 + 8 + 4 = 44
+
+        Bitwise AND - &
+        Bitwise OR - ||
+        Bitwise NOT - ~ (tilde)
+        Bitwise XOR - ^ (caret)
+        XOR - 00 = 0, 01 = 1, 10 = 1, 11 = 0
+        Bitwise left shift <<
+        Bitwise right shift >>
+
+    */
+
+    std::cout << (10 & 2) << std::endl;
+    /*
+        1 0 1 0
+        0 0 1 0
+        -------
+        0 0 1 0 === 2
+        -------
+    */
+    std::cout << (10 | 2) << std::endl;
+    std::cout << (~10) << std::endl;
+    std::cout << (10 ^ 2) << std::endl;
+    std::cout << (10 << 1) << std::endl;
+    std::cout << (10 << 3) << std::endl;
+    /* 
+        000000000000000000 1010 = 1010
+        000000000000000001 0100 = 10100 = 20
+        000000000000000101 0000 = 1010000 = 64 + 16 = 80
+    */
+    std::cout << (10 >> 1) << std::endl;
+    std::cout << (10 >> 3) << std::endl;
+    /*
+        000000000000000000 1010 = 1010
+        000000000000000000 0101 = 0101 = 4 + 1 = 5  
+        000000000000000000 0001 = 0001 = 1
+    */
+
 }
