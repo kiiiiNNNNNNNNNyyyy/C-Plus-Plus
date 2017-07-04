5f30740277e06ee2e2fd4792c4c99dcfa5959110
Arjun  Dass <ad.sam007@gmail.com>
2017-07-03 23:54:01 -0500

    Added Enum Program

--- Basic Programs/Enum.cpp
+++ Basic Programs/Enum.cpp
@@ -2,11 +2,43 @@
 
 /* enum stands for enumeration  - 0 1 2 3 */
 
-enum dayOfWeek { M, TU, W, TH, F, SA, S };
+enum dayOfWeek { M = 1, TU, W, TH, F, SA, S };
 
+std::string getDay(dayOfWeek);
 int main(){
     dayOfWeek d = M;
 
-    std::cout << d << endl;
+    std::cout << d << std::endl;
+    std::cout << dayOfWeek(M) << std::endl;
+    std::cout << getDay(M) << std::endl;
     return 0;
+    //WE CANNOT DO CIN FOR ENUM
+}
+
+std::string getDay(dayOfWeek d){
+    switch(d){
+        case M: 
+            return "Monday";
+        
+        case TU: 
+            return "Tuesday";
+
+        case W: 
+            return "Wednesday";
+        
+        case TH:
+            return "Thursday";
+        
+        case F: 
+            return "Friday";
+
+        case SA:
+            return "Saturday";
+
+        case S:
+            return "Sunday";
+
+        default: 
+            return "You typed something wrong";
+    }
 }
