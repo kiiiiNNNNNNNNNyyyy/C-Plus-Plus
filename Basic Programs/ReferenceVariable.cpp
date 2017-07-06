a695021475f74c32d9ea88e9b676ed4e17012974
Arjun  Dass <ad.sam007@gmail.com>
2017-07-05 23:31:55 -0500

    Completed reference Variable

--- Basic Programs/ReferenceVariable.cpp
+++ Basic Programs/ReferenceVariable.cpp
@@ -3,6 +3,7 @@
 /* referenced variable 
     Reference varuable must be initialized when created 
     Reference variable can change associations
+    Reference variable must be the same type as the association
  */
 
 int main(){
@@ -10,6 +11,8 @@ int main(){
     std::string name = "Arjun";
     std::cout << "Value of variable: " << name << " " << &name << std::endl;
     std::string &ref = name;
+
+    //If i use const it wont work, coz I am reinitializing at the bottom
     std::cout << "Value of variable: " << ref << " " << &ref << std::endl;
 
     ref = "Scot";
