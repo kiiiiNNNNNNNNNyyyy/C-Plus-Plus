99b4caac5cce5bc0cd6ad3a5ff02667759aeb8c8
Arjun  Dass <ad.sam007@gmail.com>
2017-07-12 23:22:13 -0500

    Classes with polymorphism

--- Basic Programs/Polymorphism.cpp
+++ Basic Programs/Polymorphism.cpp
@@ -15,9 +15,69 @@ class Animal{
         };
 };
 
+class Dog : public Animal{
+    std::string sign;
+    public:
+        Dog(){
+            sign = "D";
+            voice = "Bow Bow";
+        }
+        ~Dog(){
+
+        }
+        std::string getVoice(){
+            return voice + " " + sign;
+        }
+};
+
+class Cat : public Animal{
+    std::string sign;
+    public: 
+        Cat(){
+            sign = "C";
+            voice = "Meow Meow";
+        }
+        ~Cat(){
+
+        }
+        std::string getVoice(){
+            return voice + " " + sign;
+        }
+};
+
+class Cow : public Animal{
+    std::string sign;
+    public: 
+        Cow(){
+            sign = "CO";
+            voice = "Moo Moo";
+        }
+        ~Cow(){
+
+        }
+        std::string getVoice(){
+            return voice + " " + sign ;
+        }
+};
+
+void voiceOfAnimal(Animal *);
 int main(){
 
     Animal animal;
     std::cout << animal.getVoice() << std::endl;
+
+    Cat cat;
+    Dog dog;
+    Cow cow;
+
+    std::cout << dog.getVoice() << std::endl;
+    std::cout << cat.getVoice() << std::endl;
+    std::cout << cow.getVoice() << std::endl;
+
+    voiceOfAnimal(&animal);
     return 0;
+}
+
+void voiceOfAnimal(Animal *p){
+    std::cout << p->getVoice()  << std::endl;
 }
