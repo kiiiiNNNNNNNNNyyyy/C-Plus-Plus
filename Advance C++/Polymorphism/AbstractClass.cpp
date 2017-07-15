56adf4bcd1b5e39d353a7c95eddd49e233b8d1f7
Arjun  Dass <ad.sam007@gmail.com>
2017-07-15 17:42:24 -0500

    using virtual function

---
+++ Advance C++/Polymorphism/AbstractClass.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+
+//Abstract class - A class that cannot be instantiated and will not be instantiated 
+// is called an abstract class
+//we create a complete abstract class by assigning a virtial function to 0;
+
+class Shape{
+    virtual void setX(int x) = 0;
+    virtual void setY(int y) = 0;
+    virtual int getX() const = 0;
+    virtual int getY() const = 0;
+    virtual void draw() const = 0;
+};
+
+class Circle : public Shape{
+    private: 
+        int x, y, radius;
+    public:
+        Circle(int xcoor, int ycoor, int r){
+            x = xcoor;
+            y = ycoor;
+            radius = r;
+        }
+
+        virtual void setX(int xcoor){
+            x = xcoor;
+        }
+
+        virtual void setY(int ycoor){
+            y = ycoor;
+        }
+
+        void setRadius(int r){
+            radius = r;
+        }
+
+        virtual int getX() const {
+            return x;
+        }
+
+        virtual int getY() const {
+            return y;
+        }
+
+        virtual int getRadius() const {
+            return radius;
+        }
+
+        virtual void draw() const{
+            std::cout << "Drawing circle at : " << getX() << ", " << getY() << " with a radius  of : " << getRadius()
+            << std::endl;     
+        }
+
+};
+
+int main(){
+
+    Circle c1(2,3,5);
+    c1.draw();
+    return 0; 
+}
