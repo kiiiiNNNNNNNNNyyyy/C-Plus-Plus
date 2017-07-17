0b7d4de647ac15232643781c2422499cf7c9a624
Arjun  Dass <ad.sam007@gmail.com>
2017-07-16 22:15:42 -0500

    Implemented quadilateral exercise

--- Advance C++/Inheritance/Exercise2.cpp
+++ Advance C++/Inheritance/Exercise2.cpp
@@ -18,11 +18,21 @@ class Quadilateral{
 
 class Trapezoid : public Quadilateral{
     public:
-        Trapezoid(double s1, double s2, double s3, double s4){
-            Quadilateral(s1, s2, s3, s4){}
-        }
+        Trapezoid(double s1, double s2, double s3, double s4) : Quadilateral(s1, s2, s3, s4){}
 };
 
+class Square : public Quadilateral{
+    public: 
+        Square(double side): Quadilateral(side, side, side, side){};
+};
+
+
+
 int main(){
+
+    Trapezoid t1(3, 5, 5, 2);
+    Square s1(6);
+    t1.display();
+    s1.display();
     return 0;
 }
