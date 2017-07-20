93a2d1c8e399b2f4f397e7ac6253025cb878ab99
Arjun  Dass <ad.sam007@gmail.com>
2017-07-19 23:36:24 -0500

    Tackled problem with template specialization

--- Advance C++/Generic Programming/ClassDefination.cpp
+++ Advance C++/Generic Programming/ClassDefination.cpp
@@ -18,11 +18,43 @@ class Stack{
 
         T pop(){
             T val = dataStore[top];
+            dataStore[top] = 0;  
             --top;
             return val;
         }
 
         T peek(){
+            return dataStore[top];
+        }
+};
+
+// Inorder to deal with the propblem with strings (in pop functionn we are assigning to 0 which is a int)
+// we initialize a template
+
+template<>
+class Stack<std::string> {
+    private:
+        std::string dataStore[100];
+        int top;
+    
+    public:
+        Stack(){
+            top = -1;
+        }
+
+        void push(std::string val){
+            ++top;
+            dataStore[top] = val;
+        }
+
+        std::string pop(){
+            std::string val = dataStore[top];
+            dataStore[top] = "";  
+            --top;
+            return val;
+        }
+
+        std::string peek(){
             return dataStore[top];
         }
 };
@@ -50,6 +82,17 @@ int main(){
 
     double value2 = numbers2.pop();
     std::cout << numbers2.peek() << std::endl;
+
+    Stack<std::string> names;
+    names.push("Mary");
+    names.push("Joe");
+    names.push("Dank");
+    names.push("Memes");
+
+    std::cout << names.peek() << std::endl;
+
+    std::string name = names.pop();
+    std::cout << names.peek() << std::endl;
 
     return 0;
 }
