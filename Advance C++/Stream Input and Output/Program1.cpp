c24d9505ee7d07ea267a0ec195ecf5a05d8ad6be
Arjun  Dass <ad.sam007@gmail.com>
2017-07-23 17:29:54 -0500

    Stream input

--- Advance C++/Stream Input and Output/Program1.cpp
+++ Advance C++/Stream Input and Output/Program1.cpp
@@ -5,6 +5,8 @@
 //fstream = ifstream, ofstream
 
 int main(){
+
+    //Managing output stream
     std::cout << "Hello World!1" << std::endl;
     std::cout.put('h'); // put for putting individual chars
     std::cout.put('e');
@@ -15,5 +17,24 @@ int main(){
     //endl not only adds a new line but it also flushes a buffer that is part of every iostream object.
 
     std::cout  << std::endl << "Hello flush" << std::flush; // flushes the buffers
+
+    std::string  name, name2;
+    std::cout << " Enter your name : " << std::endl;
+    std::cin >> name;
+    std::cout << "Your name is  " << name << std::endl;//tHIS WILL SHOW ERROR. Wont take anything after the space.
+
+    std::cout << " Enter your name again : " << std::endl;
+    getline(std::cin, name2);   // in strngs whem you need a space, you need to switch to getline
+    std::cout << "Your name is  " << name2 << std::endl;//tHIS WILL SHOW ERROR. Wont take anything after the space.
+
+
+    //Getting character input
+    //this taken is taken from C , when strings were stored as char array
+    char c; 
+    c = std::cin.get();
+    do{
+        std::cout.put(c);
+        c = cin.get();
+    } while( !std::cin.eof());
     return 0;
 }
