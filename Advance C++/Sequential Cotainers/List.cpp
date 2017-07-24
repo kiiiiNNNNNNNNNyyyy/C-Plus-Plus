7d7fc15801eed346e2e9809f9c7e83f283676a89
Arjun  Dass <ad.sam007@gmail.com>
2017-07-23 20:47:33 -0500

    List in depth

--- Advance C++/Sequential Cotainers/List.cpp
+++ Advance C++/Sequential Cotainers/List.cpp
@@ -3,6 +3,14 @@
 
 //using namespace std;
 
+void display(std::list<std::string> lyst){
+    std::list<std::string>::iterator iter = lyst.begin();
+    while(iter != lyst.end()){
+        std::cout  << *iter << std::endl;
+        ++iter;
+    }
+}
+
 int main(){
 
     std::list<std::string> names;
@@ -22,6 +30,7 @@ int main(){
     std::cout << "first item : " << names.front() << std::endl;
     std::cout << "last item : " << names.back() << std::endl;
 
+    //Reverse a list
     names.reverse();
 
     std::list<std::string>::iterator iter2 = names.begin();
@@ -31,6 +40,8 @@ int main(){
         ++iter2 ;
     }
 
+    //Sort a list
+    names.sort();
     std::list<std::string>::iterator iter3 = names.begin();
 
     while(iter3 != names.end()){
@@ -38,7 +49,29 @@ int main(){
         ++iter3;
     }
 
-    names.sort();
+    display(names);
 
+    names.push_front("Barry");  // added to the front of th list
+    display(names);
+    std::cout << "Current Size : " << names.size() << std::endl;
+
+    names.remove("Elizabeth");  // won't remove as it does not exists
+    display(names);
+
+    names.remove("Rick");   // will remove as Rick exists
+    display(names);
+
+    names.pop_front();
+    display(names); //removes first element
+
+    names.pop_back();   //removes last element
+    display(names);
+
+    names.clear();  //removes all elements from the list
+    if(names.empty()){  //checks if the list is empty
+        std::cout << "list is empty." << std::endl;
+    }else{
+        std::cout << "list is not empty." << std::endl;
+    }
     return 0; 
 }
