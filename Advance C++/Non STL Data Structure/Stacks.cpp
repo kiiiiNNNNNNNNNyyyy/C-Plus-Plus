524a9ba6266de5966bc28f40b9a16660687076f5
Arjun  Dass <ad.sam007@gmail.com>
2017-07-28 20:13:57 -0500

    Implemented a proper string stack

--- Advance C++/Non STL Data Structure/Stacks.cpp
+++ Advance C++/Non STL Data Structure/Stacks.cpp
@@ -8,7 +8,7 @@ class Stack{
 
     public:
         Stack(){
-            top = -1; 
+            top = -1;  
         }
 
         void push(std::string value){
@@ -25,8 +25,44 @@ class Stack{
         std::string peek(){
             return dataStore[top];
         }
+
+        int count(){
+            return top + 1;
+        }
+
+        void clear(){
+            for(int i=0; i<=top-1; ++i){
+                dataStore[i] = "";
+            }
+            top = -1;
+        }
+
+        bool isEmpty(){
+            if(top == -1){
+                return true;
+            }else{
+                return false;
+            }
+        }
 };
 
 int main(){
+
+    Stack names;
+    names.push("Mary");
+    names.push("Joe");
+    names.push("Jim");
+    names.push("Tom");
+
+    std::cout << "Number of items : " << names.count() << std::endl;
+    
+    if(!names.isEmpty()){
+        names.pop();
+    }
+
+    std::cout << "Number of items after removal : " << names.count() << std::endl;
+    names.clear();
+
+    std::cout << "Number of items after removal : " << names.count() << std::endl;
     return 0;
 }
