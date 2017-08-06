5337d9d587c40a56d1597ba0bd60e00fd3394c19
Arjun  Dass <ad.sam007@gmail.com>
2017-08-06 13:10:13 -0500

    Started Implemented LiinkdList calass

--- Advance C++/Non STL Data Structure/LinkedList.cpp
+++ Advance C++/Non STL Data Structure/LinkedList.cpp
@@ -16,6 +16,52 @@ class Node{
         Node *Next(){ 
             return next;
         }
+
+        void setNext(Node *aNext){
+            next = aNext;   
+        }
+};
+
+
+class LList{
+    public: 
+        Node *head;
+        LList(){
+            head = NULL;
+        }
+        void Append(std::string data){
+            Node *newNode = new Node(data, NULL);
+            Node *tmp = head;
+            if(tmp != NULL){
+                while(tmp->Next() != NULL){
+                    tmp = tmp->Next();
+                }
+                tmp->setNext(newNode);
+            }else{
+                head = newNode;
+            }
+        }
+
+        void Delete(std::string data){
+            Node *tmp = head;
+            if(tmp == NULL){
+                return;
+            }
+            if(tmp->Next() == NULL){
+                delete tmp;
+            }else{
+                Node *prev;
+                do{
+                    if(tmp->getData() == data){
+                        break;
+                    }
+                    prev = tmp;
+                    tmp = tmp->Next();
+                }while(tmp != NULL);
+                prev->setNext(tmp->Next());
+                delete tmp;
+            }
+        }
 };
 
 int main(){
